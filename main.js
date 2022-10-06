ad3111aae0895b45c99e78a3342b82b5de83eeb5
Ronak Pandey <ronakpandeyofficial@gmail.com>
2022-10-06 21:48:21 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,21 +7,21 @@ client.on("ready", () => {
     setInterval(() => {
         config.channels.map((id) => {
             const channel = client.channels.cache.get(id)
-            const members = channel.guild.members.cache.filter(mem => mem.user.displayAvatarURL({dynamic: true}).split(".")[3] == 'gif')
+            const members = channel.guild.members.cache//.filter(mem => mem.user.displayAvatarURL({dynamic: true}).split(".")[3] == 'gif')===========
             const user = members.random().user
             const avatar = user.displayAvatarURL({ dynamic: true, size: 2048 })
             console.log(avatar)
-            if (avatar.split('.')[3].split('?')[0] == 'gif') {
+            //if (avatar.split('.')[3].split('?')[0] == 'gif') {=============
                 channel.send({
                     embeds: [{
                         author: { iconURL: avatar, name: user.tag },
                         //description: `[GIF](${avatar}) - [GIF](${avatar}) - [GIF](${user.displayAvatarURL({ size: 2048, format: "gif" })}) - [GIF](${avatar})`,
-                        footer: { text: "Made by ROИΔK#0083" },
+                        footer: { text: "Made by ROИΔK#3937" },
                         image: { url: avatar },
                         color: config.color
                     }]
                 })
-            }
+            //}=============
         })
     }, config.time)
 })
@@ -32,26 +32,28 @@ client.on("messageCreate", (message) => {
 
     if (message.content.includes("@here") || message.content.includes("@everyone") || message.type == "REPLY") return false;
 
-    if (message.mentions.has(client.user.id)) {
+    /*if (message.mentions.has(client.user.id)) {
         message.channel.send("There are not any commands, the bot only sends the profile picture of random users of guild in a specified channel.");
-    }
+    }*/
 });
 
 client.on("ready", () => {
     client.user.setPresence({
         status: "idle",
         activities: [{
-            name: "ROИΔK#0083",
+            name: "ROИΔK#3937",
             type: "LISTENING",
         }],
     });
-    /*const { joinVoiceChannel } = require('@discordjs/voice');
-    const channel = client.channels.cache.get('928896858787557437')
+
+    const { joinVoiceChannel } = require('@discordjs/voice');
+    const channel = client.channels.cache.get('1023513652806029352')
     joinVoiceChannel({
         channelId: channel.id,
         guildId: channel.guild.id,
         adapterCreator: channel.guild.voiceAdapterCreator,
-    });*/
+    });
+
 });
 
 client.login(config.token)
